eab265b14224814d95108b44ea6b32cf872302c7
John Hwang <johnhwang@college.harvard.edu>
2024-03-04 15:48:57 -0500

    update on bus component

--- src/lib/components/BusDetail.svelte
+++ src/lib/components/BusDetail.svelte
@@ -1,13 +1,45 @@
 <script lang="ts">
-
+    export let stops = [];
+    export let busLocation = 0;
 </script>
 
-
-<div>
-
-</div>
-
-
 <style>
+    .route-line {
+        stroke: black;
+        stroke-width: 2;
+    }
 
+    .stop-dot {
+        fill: red;
+    }
+
+    .stop-name {
+        font-size: 12px;
+        text-anchor: middle;
+        dominant-baseline: hanging;
+    }
+
+    .bus-icon {
+        font-size: 24px;
+    }
+
+    .container {
+        margin-left: 10px;
+    }
 </style>
+
+<div class="container">
+    <svg width="100%" height="100">
+        <!-- Route Line -->
+        <line x1="10" y1="50" x2="390" y2="50" class="route-line" />
+
+        <!-- Stops -->
+        {#each stops as stop, index}
+        <circle cx="{10 + stop.position * 380}" cy="50" r="5" class="stop-dot" />
+        <text x="{16 + stop.position * 380}" y="65" class="stop-name">{stop.name}</text>
+        {/each}
+
+        <!-- Bus Icon (we could use png) -->
+        <text x="{10 + busLocation * 380}" y="40" class="bus-icon">&#128652;</text>
+    </svg>
+</div>
