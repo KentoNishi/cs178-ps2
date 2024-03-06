5743fea2c5d609eea6673081a762fe5df7ac5770
Kento Nishi <kento24gs@outlook.com>
2024-03-06 16:27:45 -0500

    +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import MobileWrapper from "$lib/components/MobileWrapper.svelte";
+  </script>
+<div></div>
+
+<MobileWrapper>
+  <slot />
+</MobileWrapper>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,5 @@
 <script>
-  import MobileWrapper from "$lib/components/MobileWrapper.svelte";
   import RouteViewer from "$lib/components/RouteResults.svelte";
 </script>
-<div></div>
 
-<MobileWrapper>
-  <RouteViewer />
-</MobileWrapper>
+<RouteViewer />
