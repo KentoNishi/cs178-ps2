7a93174df8d084a0473f359d92684236883476ed
Kento Nishi <kento24gs@outlook.com>
2024-03-06 21:16:58 +0000

    wip mobile wrap

---
+++ src/lib/components/MobileWrapper.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<div class="wrapper">
+  <div class="mobile">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .mobile {
+    width: 360px;
+    height: 634px;
+    background-color: red;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
-	import RouteViewer from "$lib/components/RouteResults.svelte";
+  import MobileWrapper from "$lib/components/MobileWrapper.svelte";
+  import RouteViewer from "$lib/components/RouteResults.svelte";
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div></div>
 
-<RouteViewer />
+<MobileWrapper>
+  <RouteViewer />
+</MobileWrapper>
